5eec13634467740cccf59a27dc69953586cac51e
Krishna Murthy Srirambhatla <95994492+Krishna555982@users.noreply.github.com>
2025-09-12 22:07:23 +0530

    Updated and fixed version of Order Details

--- Routes/OrderRoutes.js
+++ Routes/OrderRoutes.js
@@ -1,8 +1,13 @@
-const expres = require('express')
-const router = expres.Router();
+const express = require('express')
+const router = express.Router();
 const OrderController = require("../controllers/OrderCotroller")
 
 router.post("/addOrderDetails",OrderController.addOrderDetails)
 
+router.put('/updateOrderDetails',OrderController.updateOrderDetails)
+
+router.get('/getOrderDetails',OrderController.getOrderDetails)
+
+router.delete('/deleteOrderDetails',OrderController.deleteOrderDetails)
 
 module.exports = router;
--- controllers/OrderCotroller.js
+++ controllers/OrderCotroller.js
@@ -4,22 +4,68 @@ const {
     messages,
 } = require("../constant/responseConstant")
 const OrderSchema = require("../models/OrderSchema");
-const productSchema = require("../models/productSchema");
 
 //Add Order Details Here
-
-exports.addOrderDetails = async(req,res) => {
+exports.addOrderDetails = async(req,res) =>{
     try {
-        console.log(req.body)
-        const { ItemName, ItemPrice, ItemModel, OrderStatus } = req.body;
+        const { ItemName , ItemPrice , ItemModel , OrderStatus } = req.body;
         if(!ItemName || !ItemPrice || !ItemModel || !OrderStatus) {
-            return res.status(statusCodes.BAD_REQUEST).json({message: "Fill All The Details"})
-        } else {
-            const addDetails = await OrderSchema.create(req.body)
-            console.log ("hi",addDetails)
-            return res.status(statusCodes.OK).json({message: "API Created Successfully"})
+            return res.status(statusCodes.BAD_REQUEST).json({message: "Please Insert the Data"});
+        }
+        const addOrderDetails = await OrderSchema.create(req.body)
+        console.log(addOrderDetails)
+            return res.status(statusCodes.OK).json({message: "Order Details Added Successfully"});
+    } catch(error) {
+        return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({message: error})
     }
-} catch(error) {
-    console.log("Hello")
-        return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({message: "Internal Server Error"})    }
+}
+
+// Update Order Details in the Database
+exports.updateOrderDetails = async(req,res) => {
+    try {
+        const update = await OrderSchema.findOneAndUpdate({ItemName: req.body.ItemName}, {$set: {OrderStatus: req.body.OrderStatus}}, {new: true})
+        console.log("Updated",update)
+        return res.status(statusCodes.OK).json({message: "Order Details Updated Successfully"})
+    } catch(error) {
+        return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({message: error})
+    }
+}
+
+//Get Order Details From the DataBase
+
+exports.getOrderDetails = async(req,res) => {
+    try {
+        const getOrderDetails = await OrderSchema.find({ItemName: req.body.ItemName})
+        console.log(getOrderDetails)
+        if(getOrderDetails){
+            return res.status(statusCodes.OK).json({message: getOrderDetails})
+        }
+        else {
+            return res,status(statusCodes.BAD_REQUEST).json({message: "There is No Data With the provided Details"})
+        }
+    }
+    catch (error) {
+        return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({message: "Internal Server Error"})
+    }
+}
+
+// Deleting Order Details From DataBase
+
+exports.deleteOrderDetails = async(req,res) => {
+    try {
+        if(req.body.ItemName) {
+            const deleteOrderDetails = await OrderSchema.deleteOne({ItemName : req.body.ItemName})
+            console.log("The Data Is Deleted",deleteOrderDetails)
+            if(deleteOrderDetails.deletedCount == 1) {
+                return res.status(statusCodes.OK).json({message:"The Details Got Deleted"})
+        }
+            else {
+                return res.status(statusCodes.NOT_FOUND).json({message: "No Data Found"})
+            }
+    } else {
+        return res.status(statusCodes.NOT_FOUND).json({message: "Item Name is Not Found"})
+    }
+} catch (error) {
+    return res.status(statusCodes.INTERNAL_SERVER_ERROR).json({message: "Internal Server Error"})
+}
 }
